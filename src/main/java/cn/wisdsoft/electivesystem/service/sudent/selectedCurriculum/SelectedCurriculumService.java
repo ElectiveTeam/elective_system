bfaf7fdb27ea98537be62b805b37df77b20afcb4
lizetuty <136229899@qq.com>
2019-01-09 08:04:48 +0800

    学生建包3

---
+++ src/main/java/cn/wisdsoft/electivesystem/service/sudent/selectedCurriculum/SelectedCurriculumService.java
@@ -0,0 +1,11 @@
+package cn.wisdsoft.electivesystem.service.sudent.selectedCurriculum;
+
+/**
+ * @ClassName SelectedCurriculumService
+ * @Description
+ * @Author LIZEYU
+ * @Date 2019/1/9 7:55
+ * @Version 1.0
+ **/
+public interface SelectedCurriculumService {
+}
---
+++ src/main/java/cn/wisdsoft/electivesystem/service/sudent/selectedCurriculum/impl/SelectedCurriculumServiceImpl.java
@@ -0,0 +1,13 @@
+package cn.wisdsoft.electivesystem.service.sudent.selectedCurriculum.impl;
+
+import cn.wisdsoft.electivesystem.service.sudent.selectedCurriculum.SelectedCurriculumService;
+
+/**
+ * @ClassName SelectedCurriculumServiceImpl
+ * @Description
+ * @Author LIZEYU
+ * @Date 2019/1/9 7:57
+ * @Version 1.0
+ **/
+public class SelectedCurriculumServiceImpl implements SelectedCurriculumService {
+}
